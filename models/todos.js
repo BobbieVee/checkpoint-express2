c0dd9982c1d7c329b09014dee6af889a279f7f0f
Robert van der Mark <robert.vandermark@gmail.com>
2017-02-09 14:25:54 -0500

    passed model tests

--- models/todos.js
+++ models/todos.js
@@ -1,17 +1,33 @@
 'use strict';
 
-var tasks = {}; // a place to store tasks by person
+var tasks = {}; 
 
 module.exports = {
   reset: function () {
-    tasks = {}; // (this function is completed for you.)
+    tasks = {}; 
   },
-  // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
+
   listPeople: function () {
-    // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
   },
   add: function (name, task) {
-    // saves a task for a given person
+      if (!task['complete']) task['complete'] = false;
+      if (!tasks[name]) {
+        tasks[name]= [task];
+      } else {
+        tasks[name].push(task);
+      }  
+  },
+
+  complete: function(name, index){
+    tasks[name][index].complete = true;
+  },
+
+  list: function (name){
+    return tasks[name];
+  }, 
+
+  remove: function(name, index){
+    tasks[name].splice(index,1);
   }
-  // etc.
 };
